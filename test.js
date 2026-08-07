f6630f6a01b1625b04e6dac7db839a5b74412600
isithore002 <isithore002@users.noreply.github.com>
2026-08-07 08:29:48 +0530

    feat: add security token test script and force IPv4 DNS resolution for dev environment

---
+++ test.js
@@ -0,0 +1,1 @@
+fetch("https://api.gopluslabs.io/api/v1/token_security/8453?contract_addresses=0xed0081BB40b7Bf64D407Ec25a99475d0BB8ed903").then(r => r.json()).then(console.log).catch(console.error)
